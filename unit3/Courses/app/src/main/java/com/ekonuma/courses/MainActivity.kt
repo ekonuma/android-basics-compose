58b39e2c3802793a402cf7533e95ece3814f886d
ekonuma <konumaeike5618@gmail.com>
2024-05-27 23:14:09 -0300

    refactor(change shapes config)

--- unit3/Courses/app/src/main/java/com/ekonuma/courses/MainActivity.kt
+++ unit3/Courses/app/src/main/java/com/ekonuma/courses/MainActivity.kt
@@ -9,20 +9,29 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ekonuma.courses.data.DataSource
 import com.ekonuma.courses.model.Topic
 import com.ekonuma.courses.ui.theme.CoursesTheme
@@ -33,10 +42,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoursesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    DataSource.topics.forEach {
-                        TopicGrid(modifier = Modifier.padding(innerPadding))
-                    }
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TopicGrid(
+                        modifier = Modifier.padding(
+                            start = dimensionResource(R.dimen.padding_small),
+                            top = dimensionResource(R.dimen.padding_small),
+                            end = dimensionResource(R.dimen.padding_small),
+                        )
+                    )
                 }
             }
         }
@@ -59,19 +77,42 @@ fun TopicGrid(modifier: Modifier = Modifier) {
 
 @Composable
 fun Topic(topic: Topic, modifier: Modifier = Modifier) {
-    val picture = painterResource(id = topic.picture)
-    Box(modifier = modifier) {
-        Column {
-            Image(
-                painter = picture,
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Fit
-            )
-        }
-        Column {
-            Row {
-                Text(text = stringResource(id = topic.title))
+    Card {
+        Row {
+            Box(modifier = modifier) {
+                Image(
+                    painter = painterResource(id = topic.picture),
+                    contentDescription = null,
+                    modifier = modifier
+                        .size(width = 68.dp, height = 68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Column {
+                Text(
+                    text = stringResource(id = topic.title),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = modifier.padding(
+                        start = 16.dp,
+                        top = 16.dp,
+                        end = 16.dp,
+                        bottom = 8.dp
+                    )
+                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = dimensionResource(R.dimen.padding_medium))
+                    )
+                    Text(
+                        text = topic.coursesCount.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_small))
+                    )
+                }
             }
         }
     }
